37df1016486ab6c14a95440dfc4a70f7c139c645
omunam <omunam10@gmail.com>
2021-05-16 17:26:42 -0400

    adding notes to test github and gnome-keyring compatibility

--- 3-Github2fa-Setup.sh
+++ 3-Github2fa-Setup.sh
@@ -2,4 +2,6 @@
 # Setup Github 2fa with gnome-keyring
 # https://marklodato.github.io/2013/10/25/github-two-factor-and-gnome-keyring.html
 
+# next time on pushing to a repo
+# just enter your access token instead of password and your good
 git config --global credential.helper gnome-keyring
