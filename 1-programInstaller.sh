b9497b0744ad8b71aab092a0df05d9206ff6c60b
MuazAlhaidar <omunam10@gmail.com>
2021-05-19 17:42:16 -0400

    Update 1-programInstaller.sh

--- 1-programInstaller.sh
+++ 1-programInstaller.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
+sudo pacman -Syu
+
 sudo pacman -Sy netbeans discord htop libreoffice-still lutris openshot rhythmbox steam base-devel atom handbrake git gitg gnome-keyring libsecret seahorse speedtest-cli jre-openjdk nodejs npm python python-pipx pycharm-community-edition
 
 # Download Whatsapp for linux off of the manjaro software store
