5944a578e85cbced2b9212982763cae3f4228b6c
MuazAlhaidar <omunam10@gmail.com>
2021-08-22 11:49:40 -0400

    decreasing script size, adding packages, and adding i3 as a environment

--- 1-ProgramInstaller.sh
+++ 1-ProgramInstaller.sh
@@ -1,51 +1,13 @@
 #! /usr/bin/bash
 
 sudo pacman -Syu 
-sudo pacman -S netbeans --noconfirm
-sudo pacman -S discord --noconfirm
-sudo pacman -S htop --noconfirm
-sudo pacman -S libreoffice-still --noconfirm
-sudo pacman -S lutris --noconfirm
-sudo pacman -S openshot --noconfirm
-sudo pacman -S rhythmbox --noconfirm
-sudo pacman -S steam --noconfirm
-sudo pacman -S base-devel --noconfirm
-sudo pacman -S atom --noconfirm
-sudo pacman -S handbrake --noconfirm
-sudo pacman -S git --noconfirm
-sudo pacman -S gitg --noconfirm
-sudo pacman -S gnome-keyring --noconfirm
-sudo pacman -S libsecret --noconfirm
-sudo pacman -S seahorse --noconfirm
-sudo pacman -S speedtest-cli --noconfirm
-sudo pacman -S jre-openjdk --noconfirm
-sudo pacman -S nodejs --noconfirm
-sudo pacman -S npm --noconfirm
-sudo pacman -S python --noconfirm
-sudo pacman -S python-pipx --noconfirm
-sudo pacman -S pycharm-community-edition --noconfirm
-sudo pacman -S kalarm --noconfirm
-sudo pacman -S clamav --noconfirm
-sudo pacman -S gimp --noconfirm
-sudo pacman -S blender --noconfirm
-sudo pacman -S appimagelauncher --noconfirm
-sudo pacman -S gnome-tweaks --noconfirm
-sudo pacman -S ntfs-3g --noconfirm
-sudo pacman -Sy manjaro-settings-manager --noconfirm
+sudo pacman -S netbeans discord htop libreoffice-still lutris openshot rhythmbox steam base-devel atom handbrake git gitg gnome-keyring libsecret seahorse speedtest-cli jre-openjdk nodejs npm python python-pipx pycharm-community-edition kalarm clamav gimp blender appimagelauncher gnome-tweaks ntfs-3g manjaro-settings-manager lxappearance arandr qt5ct i3 powerline --noconfirm
 
 # Download Whatsapp for linux off of the manjaro software store
 
-git clone https://aur.archlinux.org/trizen.git
-cd trizen
-makepkg -si --noconfirm
-
-# Have to be manually installed as trizen cant do multiple packages at the same time
-trizen visual-studio-code-bin 
-trizen bluejeans 
-trizen zoom 
-trizen elementary-planner 
-trizen python-fangfrisch 
-trizen ttf-ms-fonts
+# yay allows multiple package installs at the same time
+yay -Syu
+yay -S visual-studio-code-bin bluejeans zoom elementary-planner python-fangfrisch ttf-ms-fonts drawio-desktop libopenaptx powerline-fonts-git 
 
 sudo freshclam
 
--- 2-WoWandWine.sh
+++ 2-WoWandWine.sh
@@ -1,55 +1,10 @@
 #! /usr/bin/bash
 
-sudo pacman -S wine-staging --noconfirm
-sudo pacman -S giflib --noconfirm
-sudo pacman -S lib32-giflib --noconfirm
-sudo pacman -S libpng --noconfirm
-sudo pacman -S lib32-libpng --noconfirm
-sudo pacman -S libldap --noconfirm
-sudo pacman -S lib32-libldap --noconfirm
-sudo pacman -S gnutls --noconfirm
-sudo pacman -S lib32-gnutls --noconfirm
-sudo pacman -S mpg123 --noconfirm
-sudo pacman -S lib32-mpg123 --noconfirm
-sudo pacman -S openal --noconfirm
-sudo pacman -S lib32-openal --noconfirm
-sudo pacman -S v4l-utils --noconfirm
-sudo pacman -S lib32-v4l-utils --noconfirm
-sudo pacman -S libpulse --noconfirm
-sudo pacman -S lib32-libpulse --noconfirm
-sudo pacman -S libgpg-error --noconfirm
-sudo pacman -S lib32-libgpg-error --noconfirm
-sudo pacman -S alsa-plugins --noconfirm
-sudo pacman -S lib32-alsa-plugins --noconfirm
-sudo pacman -S alsa-lib --noconfirm
-sudo pacman -S lib32-alsa-lib --noconfirm
-sudo pacman -S libjpeg-turbo --noconfirm
-sudo pacman -S lib32-libjpeg-turbo --noconfirm
-sudo pacman -S sqlite --noconfirm
-sudo pacman -S lib32-sqlite --noconfirm
-sudo pacman -S libxcomposite --noconfirm
-sudo pacman -S lib32-libxcomposite --noconfirm
-sudo pacman -S libxinerama --noconfirm
-sudo pacman -S lib32-libgcrypt --noconfirm
-sudo pacman -S libgcrypt --noconfirm
-sudo pacman -S lib32-libxinerama --noconfirm
-sudo pacman -S ncurses --noconfirm
-sudo pacman -S lib32-ncurses --noconfirm
-sudo pacman -S opencl-icd-loader --noconfirm
-sudo pacman -S lib32-opencl-icd-loader --noconfirm
-sudo pacman -S libxslt --noconfirm
-sudo pacman -S lib32-libxslt --noconfirm
-sudo pacman -S libva --noconfirm
-sudo pacman -S lib32-libva --noconfirm
-sudo pacman -S gtk3 --noconfirm
-sudo pacman -S lib32-gtk3 --noconfirm
-sudo pacman -S gst-plugins-base-libs --noconfirm
-sudo pacman -S lib32-gst-plugins-base-libs --noconfirm
-sudo pacman -S vulkan-icd-loader --noconfirm
-sudo pacman -S lib32-vulkan-icd-loader --noconfirm
+sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
+lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
+sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
+ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 \
+lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
 
-sudo pacman -S lib32-gnutls --noconfirm
-sudo pacman -S lib32-libldap --noconfirm
-sudo pacman -S lib32-libgpg-error --noconfirm
-sudo pacman -S lib32-sqlite --noconfirm
-sudo pacman -S lib32-libpulse --noconfirm
+sudo pacman -S lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse lib32-alsa-plugins --noconfirm
